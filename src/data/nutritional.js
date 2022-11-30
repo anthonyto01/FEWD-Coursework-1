2e967fb3e600805f773e0b04e323c3d4955b51da
ATO <58954176+anthonyto01@users.noreply.github.com>
2022-11-30 18:56:15 +0000

    Updated Nutritional

--- src/data/nutritional.js
+++ src/data/nutritional.js
@@ -1,20 +1,17 @@
 export const nutritional= [
+    
 {
-    nutritional_information: [
-        {
-            sugar_g:"",
-            fiber_g:"",
-            serving_size_g:"",
-            sodium_mg:"",
-            name:"pork",
-            potassium_mg:"",
-            fat_saturated_g:"",
-            fat_total_g:"",
-            calories:"",
-            cholesterol_mg:"",
-            protein_g:"",
-            carbohydrates_total_g:"",
-        }
-    ],
+    sugar_g:"",
+    fiber_g:"",
+    serving_size_g:"",
+    sodium_mg:"",
+    name:"pork",
+    potassium_mg:"",
+    fat_saturated_g:"",
+    fat_total_g:"",
+    calories:"",
+    cholesterol_mg:"",
+    protein_g:"",
+    carbohydrates_total_g:"",
 }
 ]
